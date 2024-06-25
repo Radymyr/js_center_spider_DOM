3582293e9f9b59b0be3a2a67898c23c31a43e907
Radymyr <loboda.radymyr@gmail.com>
2024-06-25 16:58:01 +0300

    make build

---
+++ index.4d81603b.js
@@ -0,0 +1,2 @@
+var e,t;e=".spider",(t=document.querySelector(e)).style.position="inherit",t.parentElement.style.display="flex",t.parentElement.style.justifyContent="center",t.parentElement.style.alignItems="center";
+//# sourceMappingURL=index.4d81603b.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const moveTheSpider = (selector)=>{
+    const spider = document.querySelector(selector);
+    spider.style.position = "inherit";
+    spider.parentElement.style.display = "flex";
+    spider.parentElement.style.justifyContent = "center";
+    spider.parentElement.style.alignItems = "center";
+};
+moveTheSpider(".spider");
+
+//# sourceMappingURL=index.f75de5e1.js.map
